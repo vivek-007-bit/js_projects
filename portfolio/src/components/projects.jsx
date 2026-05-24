5e1823c14044c4186425b65f500a5bdfcda884f8
Vivek Sharma <viveksharma40649@gmail.com>
2026-05-24 23:37:16 +0530

    Update projects.jsx

--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -1,38 +1,39 @@
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 function Projects() {
+
   const cards = [
     {
       id: 1,
       img: '/project1.webp',
-      text: 'A machine learning project that converts handwritten text into digital text using neural networks and image processing techniques for accurate character recognition.',
-      techStack: ['Python', 'JavaScript'],
+      text: `<h4>Handwriting2Text</h4>A ML project that converts handwritten images into digital text using neural networks and image processing.`,
+      techStack: ['JavaScript', 'Python'],
       link: 'https://img2text-tesseractjs.onrender.com/',
       github: 'https://github.com/vivek-007-bit/js_projects/tree/main/Image2Text_tesseratjs',
     },
     {
       id: 2,
       img: '/project2.png',
-      text: 'A full-stack Facebook-inspired social media application with secure authentication, interactive user features for improved user engagement.',
-      techStack: ['PHP', 'MySQL', 'Bootstrap', 'JavaScript', 'Cloudinary API'],
+      text: `<h4>SocialSphere</h4>A full-stack Facebook-inspired social media application with secure authentication, interactive user features for improved user engagement.`,
+      techStack: ['PHP', 'MySQL', 'JavaScript', 'Python'],
       link: 'https://facebook-m0pv.onrender.com/login.php',
       github: 'https://github.com/vivek-007-bit/php_projects/tree/main/facebook',
     },
     {
       id: 3,
-      img: '/project3.webp',
-      text: 'An online journaling application with secure login/signup authentication and full CRUD functionality, allowing users to create, edit, manage, and organize their personal journal entries efficiently.',
-      techStack: ['PHP', 'MySQL', 'Bootstrap', 'JavaScript'],
-      link: 'https://diary-app-4k8w.onrender.com/',
-      github: 'https://github.com/vivek-007-bit/php_projects/tree/main/diary-app',
+      img: '/project4.png',
+      text: `<h4>Nextweather</h4>A weather application that provides real time weather data using OpenWeather API and forecasts weather using Machine Learing algorithms`,
+      techStack: ['JavaScript', 'Python'],
+      link: 'https://nextweather7.netlify.app/',
+      github: 'https://github.com/vivek-007-bit/js_projects/tree/main/NextWeather',
     },
     {
       id: 4,
-      img: '/project4.png',
-      text: 'A responsive weather application that provides real-time weather forecasts with live location detection, allowing users to instantly view accurate weather updates for their current location or searched cities.',
-      techStack: [ 'Bootstrap', 'Weather API', 'JavaScript'],
-      link: 'https://nextweather7.netlify.app/',
-      github: 'https://github.com/vivek-007-bit/js_projects/tree/main/NextWeather',
+      img: '/project3.webp',
+      text: `<h4>My Diary</h4>An online journaling application with secure login/signup authentication and full CRUD functionality, allowing users to create, edit, manage, and organize their personal journal entries efficiently.`,
+      techStack: ['PHP', 'MySQL', 'JavaScript'],
+      link: 'https://diary-app-4k8w.onrender.com/',
+      github: 'https://github.com/vivek-007-bit/php_projects/tree/main/diary-app',
     },
   ];
 
@@ -52,9 +53,11 @@ function Projects() {
                   style={{ objectFit: 'cover' }}
                 />
                 <Card.Body>
-                  <Card.Text>{card.text}</Card.Text>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: card.text }}
+                  />
                   <div className="mb-3">
-                  <h6 className='mb-4 mt-5'>Tech Stack</h6>
+                    <h6 className='mb-2 mt-4'>Tech Stack</h6>
                     {card.techStack.map((tech, index) => (
                       <Button
                         key={index}
