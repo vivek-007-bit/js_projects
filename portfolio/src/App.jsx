ba3546c461004f7597506e8801fbdb83a7762d8e
Vivek Sharma <viveksharma40649@gmail.com>
2026-05-26 07:26:44 +0530

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -33,6 +33,7 @@ function App() {
           }
         />
 
+         <Route path="/hero" element={<Hero />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/certificates" element={<Certificates />} />
