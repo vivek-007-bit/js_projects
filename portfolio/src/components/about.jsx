e15ca68b3c09322637c987cb2933774ff28e940e
Vivek Sharma <viveksharma40649@gmail.com>
2026-06-02 22:14:35 +0530

    Update about.jsx

--- portfolio/src/components/about.jsx
+++ portfolio/src/components/about.jsx
@@ -62,7 +62,7 @@ function about() {
             <div className="d-grid gap-2 d-md-flex justify-content-md-start">
               <SocialLinks />
               <Button variant="outline-warning" size="lg" className="px-4 me-md-2">
-                <a href="/Vivek_Sharma_resume_final.pdf" className='text-decoration-none text-dark fw-bold' target="_blank" rel="noopener noreferrer"> Download CV</a>
+                <a href="/Vivek Sharma_resume.pdf" className='text-decoration-none text-dark fw-bold' target="_blank" rel="noopener noreferrer"> Download CV</a>
               </Button>
             </div>
 
